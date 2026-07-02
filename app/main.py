7e549291893f32d1808a9328263eaf543622fd3d
m1rua <vagnomail@mail.ru>
2026-07-02 23:11:36 +0300

    add top3 winrate avg_kda

--- app/main.py
+++ app/main.py
@@ -1,16 +1,46 @@
-from opendota import get_player_info, get_player_matches, get_player_wl
+from opendota import get_player_info, get_player_matches, get_player_wl, get_heroes
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/player/{account_id}")
 async def player_stats(account_id: int):
-    info = await get_player_info(account_id)
-    matches = await get_player_matches(account_id)
-    wl = await get_player_wl(account_id)
+    info = get_player_info(account_id)
+    matches = get_player_matches(account_id)
+    wl = get_player_wl(account_id)
+    all_heroes = get_heroes()
+
+
+    total_kills = sum(m["kills"] for m in matches)
+    total_death = sum(m["deaths"] for m in matches)
+    total_assists = sum(m["assists"] for m in matches)
+    winrate = wl["win"] / (wl["win"] + wl["lose"]) * 100
+    heroes_map = {h["id"]: h["localized_name"] for h in all_heroes}
+    avg_kda = round((total_kills + total_assists) / total_death, 2)
+
+
+    heroes = {}
+    for m in matches:
+        hero = m["hero_id"]
+        if hero not in heroes:
+            heroes[hero] = {"games": 0, "wins": 0}
+        heroes[hero]["games"] += 1
+        is_radiant = m["player_slot"] < 128
+        won = (is_radiant and m["radiant_win"]) or (not is_radiant and not m["radiant_win"])
+        if won:
+            heroes[hero]["wins"] += 1
+
+    top3 = sorted(heroes.items(), key=lambda x: x[1]["games"], reverse=True)[:3]
+    top3_named = [
+        {"hero": heroes_map.get(hero_id, hero_id), "games": stats["games"], "wins": stats["wins"]}
+         for hero_id, stats in top3
+    ]
 
     return {
         "info": info,
         "matches": matches,
-        "wl":wl
+        "wl": wl,
+        "winrate": winrate,
+        "avg_kda": avg_kda, 
+        "top3": top3_named
     }
--- app/opendota.py
+++ app/opendota.py
@@ -2,17 +2,18 @@ import httpx
 
 BASE_URL = "https://api.opendota.com/api"
 
-async def get_player_matches(account_id: int):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{BASE_URL}/players/{account_id}/matches?limit=30")
-        return response.json()
+def get_player_matches(account_id: int):
+    response = httpx.get(f"{BASE_URL}/players/{account_id}/matches?limit=30", verify=False)
+    return response.json()
 
-async def get_player_info(account_id: int):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{BASE_URL}/players/{account_id}")
-        return response.json()
+def get_player_info(account_id: int):
+    response = httpx.get(f"{BASE_URL}/players/{account_id}", verify=False)
+    return response.json()
 
-async def get_player_wl(account_id: int, limit: int = 30):
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{BASE_URL}/players/{account_id}/wl?limit={limit}")
-        return response.json()
+def get_player_wl(account_id: int, limit: int = 30):
+    response = httpx.get(f"{BASE_URL}/players/{account_id}/wl?limit={limit}", verify=False)
+    return response.json()
+
+def get_heroes():
+    response = httpx.get(f"{BASE_URL}/heroes", verify=False)
+    return response.json()
